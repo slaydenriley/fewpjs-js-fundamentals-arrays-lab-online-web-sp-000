c3da5e38450f3671caeff6f347a465fbc45f222d
Riley Slayden <slaydenriley@gmail.com>
2020-04-28 16:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const append = ["Milo", "Otis", "Garfield"]
 const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
